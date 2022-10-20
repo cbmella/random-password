e15b6fcc358bdcb0e090dd63d23de2fa3d75681c
cbmella <cbuguenomella@gmail.com>
2022-10-20 17:42:10 -0300

    add b5

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
-import '../css/app.css';
+import '../css/app.scss';
+import * as bootstrap from 'bootstrap'
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
